19c7b2aa22bd3b2ce24e03112daf23878dfad547
alewis3421 <60369233+alewis3421@users.noreply.github.com>
2025-01-13 17:25:17 -0600

    added Home page intro

--- resume-Site/src/App.jsx
+++ resume-Site/src/App.jsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <div>
       <Header />
+      <p>Hello and Welcome to my website. My name is Alexander Lewis and I'm a recent graduate from Texas 
+        State University with a Bachelor's in Computer Science, while also minoring in Technology. I'm
+        currently looking for oppurtunities in Front or Backend Development with hopes of one day being a
+        Full Stack Developer. 
+      </p>
     </div>
   )
 }
